8a338cd80aeabb322bb9e90d1ea9e12f352da775
Andrey Denisov <andrey.denisov@indeed-id.com>
2025-01-10 10:48:46 +0300

    Handmade "server" added.

---
+++ src/httpd.py
@@ -0,0 +1,38 @@
+import socket
+import threading
+
+from loguru import logger
+
+HOST = "localhost"
+PORT = 8080
+LISTEN_COUNT = 5
+ENCODING = "utf-8"
+
+
+def handle_request(client_socket: socket.socket):
+    try:
+        data = client_socket.recv(1024)
+        method = data.decode(ENCODING).split()[0]
+        if method in ["GET", "HEAD"]:
+            response = "Hi from server"
+            client_socket.sendall(response.encode(ENCODING))
+    except socket.error as e:
+        logger.error(e)
+    finally:
+        client_socket.close()
+
+
+def start_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((HOST, PORT))
+    sock.listen(LISTEN_COUNT)
+
+    while True:
+        client_socket, addr = sock.accept()
+        logger.info("Connected:", addr)
+        client_handler = threading.Thread(target=handle_request, args=(client_socket,))
+        client_handler.start()
+
+
+if __name__ == "__main__":
+    start_server()
